0295f9c2ceae9e0bbf5ddaca63488474a8f4f806
ppaing <ppaing@uvm.edu>
2018-04-08 12:52:27 -0400

    Phone April 8, 12:52 PM

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cs008FinalProject
 There's our final project on Mutual Africa Community
+
---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Phone's Fabulous Website</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Phone Htet Paing">
+        <meta name="description" content="My Thoughts About Random World Issues">
+
+    </head>
+
+    <body>
+        <figure class="right small">
+            <img alt="" src="phone-picture.jpg">
+            <figcaption>Don't forget to smile!</figcaption>
+        </figure>
+        <h1>Phone Htet Paing</h1>
+        <p><a href="tryme.php">Try it you'll like it!</a></p>
+        <p>Labs</p>
+        <ol>
+            <li><a href="lab1.php">Lab One</a></li>
+   
+        </ol>
+
+    </body>
+</html>
