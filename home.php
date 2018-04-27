3af9fbc728b9afddb816b0e6d7802adddac62175
wakazoo <wakazoo1997@gmail.com>
2018-04-27 11:37:40 -0400

    aa safsafa
    asasf

--- home.php
+++ home.php
@@ -8,24 +8,10 @@ include('top.php');
         <figcaption>Enactus</figcaption>
     </figure>
     <h2> Our Focus  </h2>
-    <ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-    </ul>  
-    <h2> Our Goals  </h2>
-    <ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-    </ul>  
+   
     
     <h2> Our Vision </h2>
-    <ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-    </ul>
+    
     
     </p>
     
@@ -34,11 +20,7 @@ include('top.php');
 </article>
 
 <aside id="other">
-    <h2>Extra info</h2>
-    <p>The higher the temperature, the greater the risk in all regions of the world, the faster the temperature rises, the more serious the damage will be. Emissions do not immediately have an impact on the climate, which can exist in the atmosphere for years or decades. Since the ocean (which absorbs heat and eventually releases at a slower rate than air) appears to have a slower impact, the surface temperature will not change immediately due to greenhouse gas emissions. As a result, climate change will continue for hundreds of years after atmospheric concentrations have stabilized.    
-
-    <p>The most reliable estimate of sea level rise is that by the end of the 21st century, sea levels will rise by 28 to 58 centimetres due to sea area expansion and melting of glaciers by the end of the 21st century, compared with the levels of 1989-1999. This will increase flooding and erosion in coastal areas.If the ice continues to melt, the sea level will rise by as much as one metre by 2100. There is now evidence that the Antarctic and Greenland ice sheets are slowly melting, raising sea levels. About 125,000 years ago, polar temperatures were well above their current temperature for a long period of time, and melting polar ice caps raised sea levels by four to six metres. The constant inertia of sea level rise will last for centuries.</P
-
+</aside>
     <?php include ('footer.php'); ?>
     <body>    
 </body>
