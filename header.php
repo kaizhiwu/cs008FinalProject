998f6d0ac08be43a0e66ba40dd911362ae72ae64
wakazoo <wakazoo1997@gmail.com>
2018-04-27 21:50:00 -0400

    www
    fww

--- header.php
+++
@@ -1,9 +0,0 @@
-<!-- ####################  Start of the header ############################# -->
-<header>
-    <a href="index.php" target="_blank">
-        <h1>
-            <img src="../images/logo.png" alt="My website logo" style="width:425px;height:62px">
-        </h1>
-    </a>
-</header>
-<!-- ####################  End of the header   ############################# -->
