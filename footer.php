7e42945e052050215aed5766e9d15a935828c04f
wakazoo <wakazoo1997@gmail.com>
2018-04-27 21:50:43 -0400

    Revert "www"
    This reverts commit 998f6d0ac08be43a0e66ba40dd911362ae72ae64.

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- ######################  Start of the Footer  ########################## -->
+<footer>
+    <p>MutuaL African Community</p>
+</footer>
+<!-- ######################  End of the Footer  ############################ -->
---
+++ nav.php
@@ -0,0 +1,59 @@
+<!--####################  Main Navigation   #############################-->
+<nav>
+    <ol>
+        <?php
+        //Repeat this if block for each menu item
+        //designed to give current page a class but also allows
+        //you to have more classes if you need them
+           print '<li class="';
+        if ($path_parts['filename'] == "home") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="home.php">Home</a>';
+        print '</li>';
+
+        print '<li class="';
+        if ($path_parts['filename'] == "about") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="about.php">About Us</a>';
+        print '</li>';
+
+        print '<li class="';
+        if ($path_parts['filename'] == "services") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="services.php">Services</a>';
+        print '</li>';
+
+        /*example of repeating */
+        print '<li class="';
+        if ($path_parts['filename'] == "events") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="events.php">Events</a>';
+        print '</li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "resources") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="resources.php">Resources</a>';
+        print '</li>';
+
+        print '<li class="';
+        if ($path_parts['filename'] == "contact") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="contact.php">Contact Us</a>';
+        print '</li>';
+        ?>
+        
+    </ol>
+</nav>
