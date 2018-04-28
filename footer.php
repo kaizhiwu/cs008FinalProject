60773d2bd391cc9f3522d2043c0c9c0fcae2079a
ppaing <ppaing@uvm.edu>
2018-04-27 21:41:42 -0400

    Revert "Newest version"
    This reverts commit bfeeeb2eacce8b1ba4e21887acb4ea632cd701a1.

--- footer.php
+++
@@ -1,5 +0,0 @@
-<!-- ######################  Start of the Footer  ########################## -->
-<footer>
-    <p>MutuaL African Community</p>
-</footer>
-<!-- ######################  End of the Footer  ############################ -->
