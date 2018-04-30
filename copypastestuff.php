7b110768d2dd9ec21b89fbe81320b75248e8d43a
Kate <katespaterson@gmail.com>
2018-04-29 22:37:31 -0400

    c

--- copypastestuff.php
+++ copypastestuff.php
@@ -1,8 +1,37 @@
 <?php
+include ('top.php');
+?>
+  <body id="resource">
+<article class="persist-area">
+    <h2>Resources</h2>
+    <ul><h3> About Refugees</h3>
+    <ol> 
+	
+	<li><a href="http://www.culturalorientation.net/learning/about-refugees">Cultural Orientation Resource Center</a>
+	<p>The Cultural Orientation Resource Center has lots of information about who refugees are as well as ways to welcome them and orientation and training resources.</p>
+	</li>
+	
+   <li><a href="http://refugees.org/vermont-colchester/">US Committee for Refugees and Immigrants</a>
+	<p>This tells more about actions being taken in our area, and around Vermont to help and understand the Vermont Refugee Resettlement Program (VRRP.</p>
+	</li>
+   
+    </ol>
+	</ul>
+	<li><a href="http://www.unhcr.org/pages/49c3646c2.html">United Nations High Commissioner for Refugees</a></li>
+	<p> </p>
+	<li><a href="https://www.aalv-vt.org/news">AALV</a></li>
+	<p> Their goal closely aligns with ours on a state and national wide level of helping new Americans smoothly transition into living in Vermont and the United States.</p>
+	<img src="../images/AALV.png" alt="AALV">
+	
+	<li><a href="http://www.culturalorientation.net/learning/about-refugees">Cultural Orientation Resource Center</a></li>
+	<p>Please explore the tables</p>
+	
+	
+</article>
+<?php
+include ('footer.php');
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+</body>
+</html>
 
