992f3d5893260d9225cf1014291754869455d724
Kate <katespaterson@gmail.com>
2018-05-01 21:02:25 -0400

    fds
    fsdfsasd

--- resource.php
+++ resource.php
@@ -7,26 +7,63 @@ include ('top.php');
     <ul><h3> About Refugees</h3>
     <ol> 
 	
-	<li><a href="http://www.culturalorientation.net/learning/about-refugees">Cultural Orientation Resource Center</a>
+	<li><a href="http://www.culturalorientation.net/learning/about-refugees">Cultural Orientation Resource Center</a></li>
 	<p>The Cultural Orientation Resource Center has lots of information about who refugees are as well as ways to welcome them and orientation and training resources.</p>
-	</li>
 	
-   <li><a href="http://refugees.org/vermont-colchester/">US Committee for Refugees and Immigrants</a>
-	<p>This tells more about actions being taken in our area, and around Vermont to help and understand the Vermont Refugee Resettlement Program (VRRP.</p>
-	</li>
+	
+	<li><a href="https://www.acf.hhs.gov/sites/default/files/orr/orr_fact_sheet_brief_history_of_orr.pdf
+">Office of Refugee Resettlement Fact Sheet</a></li>
+	<p>This is a quick overview of the program of the Office of Refugee Resettlement program, and what they have to offer. </p>
+	
+	
+	<li><a href="https://www.sevendaysvt.com/vermont/twenty-five-years-and-6300-people-later-a-vermont-refugee-report/Content?oid=2296187
+">Seven Day’s Vermont Refugee Overview
+</a></li>
+	<p>This tells the story of refugees in Vermont over the years and some details of the population we at MAC are trying to help.
+</p>
+	
    
     </ol>
-	</ul>
-	<li><a href="http://www.unhcr.org/pages/49c3646c2.html">United Nations High Commissioner for Refugees</a></li>
-	<p> </p>
+	
+	
+	<h3>State Level Refugee Information</h3>
+	
+	<ol> 
+	    <li><a href="http://refugees.org/vermont-colchester/">US Committee for Refugees and Immigrants</a>></li>
+	<p>This tells more about actions being taken in our area, and around Vermont to help and understand the Vermont Refugee Resettlement Program (VRRP).</p>
+	
+	<li><a href="http://humanservices.vermont.gov/departments/office-of-the-secretary/state-refugee-coordinator
+">Agency of Human Services, Vermont
+</a></li>
+	<p>This gives background about the start resettlement program and also gives information about federal services and contact information to Denise Lamoureux, the Vermont State Refugee Coordinator.   </p>
+	
+	<li><a href="http://www.healthvermont.gov/health-professionals-systems/hospitals-health-systems/refugee-health ">Vermont Department of Health
+</a></li>
+	<p>This tells about the Refugee Health Program which is set in place to assist refugees in navigating the healthcare system.
+</p>
 	<li><a href="https://www.aalv-vt.org/news">AALV</a></li>
 	<p> Their goal closely aligns with ours on a state and national wide level of helping new Americans smoothly transition into living in Vermont and the United States.</p>
-	<img src="../images/AALV.png" alt="AALV">
+	<img src="images/AALV.png" alt="AALV">
+	</ol>
 	
-	<li><a href="http://www.culturalorientation.net/learning/about-refugees">Cultural Orientation Resource Center</a></li>
-	<p>Please explore the tables</p>
+	</ul>
+    
+    <h3>National Level Refugee Information</h3>
 	
+	<ol> 
+	    <li><a href="http://refugees.org/vermont-colchester/">US Committee for Refugees and Immigrants</a>	</li>
+	<p>This tells more about actions being taken in our area, and around Vermont to help and understand the Vermont Refugee Resettlement Program (VRRP).</p>
+
+<li><a href="https://www.acf.hhs.gov/orr
+">US Office of Refugee Resettlement
+</a></li>
+	<p> Helps refugees reach their potential with services such as; Unaccompanied Child Services, Asylee Eligibility and Refugee Health.
+.</p>
+
+	</ol>
 	
+	</ul>
+    
 </article>
 <?php
 include ('footer.php');
